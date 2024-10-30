74a21d489e51c0666d0f0ea54861f1c2dad4104d
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-31 00:42:44 +0200

    Fix db name

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from pydantic.v1 import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI in Details"
 
-    DATABASE_URL: str | None = "sqlite+aiosqlite:///./cheese_catalog.db"
+    DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_temperature.db"
 
     class Config:
         case_sensitive = True
